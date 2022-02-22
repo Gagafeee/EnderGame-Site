21b78162ac6a79cbaa0e7f964e5c1a14cefd23db
Gagafeee <Gabriel.courroux@lilo.org>
2022-02-22 22:39:53 +0100

    add home page

---
+++ menu.js
@@ -0,0 +1,28 @@
+var loaded = false;
+function GetServer(isOpen){loaded = true}
+
+
+setTimeout(() => {
+       console.log(loaded); 
+       UpdateStatue();
+}, 5000);
+
+function UpdateStatue()
+{
+    round = document.getElementById("round");
+    text = document.getElementById("server-statue-text");
+
+    switch (loaded){
+        case false:{
+            round.className = "offline";
+            text.innerHTML = "Offline";
+            break;
+        }
+        case true:{
+            round.className = "online";
+            text.innerHTML = "Online";
+            break;
+        }
+        
+    }
+}
